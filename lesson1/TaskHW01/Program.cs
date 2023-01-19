a0a9d3fc2071ee10723bfc206f2ca7129103757d
sergeiguzievatiy <122722792+sergeiguzievatiy@users.noreply.github.com>
2023-01-19 20:44:07 +0300

    добавляю все 4 задания

---
+++ lesson1/TaskHW01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа 
+//и выдаёт, какое число большее, а какое меньшее.
+System.Console.WriteLine("Введите первое число> ");
+int firstNumer = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число> ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+ 
+if(firstNumer > secondNumber)
+{
+  System.Console.WriteLine(firstNumer + " Большее, а " + secondNumber + " Меньшее"  );
+}
+else
+{
+  System.Console.WriteLine(secondNumber + " Большее, а " + firstNumer + " Меньшее");
+}
---
+++ lesson1/TaskHW04/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+System.Console.Write("Введите число > ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+if (number >= count)
+{
+    System.Console.Write($"У числа {number} есть чётные числа: ");
+    while (count <= number)
+    {
+        System.Console.Write($"({count}) ");
+        count+=2;
+    }
+}
+
+else
+{
+    System.Console.WriteLine($"У числа {number} нет чётных чисел");
+}
