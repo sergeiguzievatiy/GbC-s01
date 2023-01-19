35419831bbda61df088d26501cd2713f7d08292f
sergeiguzievatiy <122722792+sergeiguzievatiy@users.noreply.github.com>
2023-01-19 11:21:43 +0300

    first проба

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2 Hello, World!");
+Console.Write("Введите первое число > ");
+int firstNumber = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите второе число > ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber * firstNumber == secondNumber)
+{
+Console.WriteLine($"Первое число {firstNumber} является квадратом второго числа {secondNumber}");
+}
+else
+{
+  Console.WriteLine($"Первое число {firstNumber} не является квадратом второго числа {secondNumber}");
+}
+
+  
