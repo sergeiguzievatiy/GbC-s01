9fe08f6ecb64299ddcd5b7cb241aa85da8b33dc7
sergeiguzievatiy <122722792+sergeiguzievatiy@users.noreply.github.com>
2023-01-24 18:36:19 +0300

    сделал задание 2

---
+++ Lesson2/Task02/Program.cs
@@ -0,0 +1,36 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+//  Пример:
+//  645 -> 5
+//  78 -> третьей цифры нет
+//  32679 -> 6
+
+int InputInt(string message)
+{
+    System.Console.Write(message + " > ");
+    string? inputValue = System.Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+bool ValidateNumberRank3(int number)
+{
+    if (number >= 100)
+    {
+        return true;
+    }
+    System.Console.WriteLine($"Число {number} не является 3-хзначным и более числом");
+    return false;
+}
+
+int number = InputInt("Введите 3-хзначное и более число");
+if (ValidateNumberRank3(number))
+{
+    int numberRank3 = number;
+    while (numberRank3 >= 1000)
+    {
+        numberRank3 = numberRank3 / 10;
+    }
+    int result = numberRank3 % 10;
+    System.Console.WriteLine($"Вторая цифра числа {number} равна ({result})");
+}
