4e74d9f7d60d96f45d85bfaca6781074381870eb
sergeiguzievatiy <122722792+sergeiguzievatiy@users.noreply.github.com>
2023-02-05 18:04:39 +0300

    do task02

---
+++ Lesson5/Task02/Program.cs
@@ -0,0 +1,45 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+//  Пример:
+//  [3, 7, 23, 12] -> 19
+//  [-4, -6, 89, 6] -> 0
+
+int[] array = CreatArray(4);
+PrintArray(array);
+
+int[] CreatArray(int size)      //создаём массив генератором случайных чисел
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-99, 99);
+    }
+    return array;
+}
+
+int Sum(int sum)
+{
+    for (int i = 0; i < array.Length; i += 2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+void PrintArray(int[] array)    //выводим результат массива
+{
+    int result = 0;
+    int sum = 0;
+    System.Console.Write("В массиве [");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            result++;
+        System.Console.Write(array[i]);
+        if (i != array.Length - 1)
+            System.Console.Write(", ");
+    }
+    sum = Sum(sum);
+    System.Console.WriteLine($"] сумма элементов на нечётных адресах равна: {sum}");
+}
