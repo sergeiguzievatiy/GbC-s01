6d47605bb8b1c38681100e36e3afd29aadec1c17
sergeiguzievatiy <122722792+sergeiguzievatiy@users.noreply.github.com>
2023-02-05 17:18:58 +0300

    сделал первое задание

---
+++ Lesson4/Task01/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+//  Пример:
+//  3, 5 -> 243 (3⁵)
+//  2, 4 -> 16
+
+double x = InputInt("Введите число");
+double degree = InputInt("Введите степень");
+double raise = 0;
+RaiseDegree(raise);
+
+double InputInt(string message)    //вводим необходимые данные
+{
+    System.Console.Write(message + " > ");
+    string? inputValue = System.Console.ReadLine();
+    double result = Convert.ToDouble(inputValue);
+    return result;
+}
+
+void RaiseDegree(double raise)  //функция возведения в степень
+{
+    raise = Math.Pow(x, degree);
+    System.Console.WriteLine($"Число {x} в степени {degree} равно: {raise}");
+}
