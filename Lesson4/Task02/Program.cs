e51126b5dff4b1fd9df7901d92bfadde5e460397
sergeiguzievatiy <122722792+sergeiguzievatiy@users.noreply.github.com>
2023-02-05 17:49:39 +0300

    сделал задание 2

--- Lesson4/Task02/Program.cs
+++ Lesson4/Task02/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+//  Пример:
+//  452 -> 11
+//  82 -> 10
+//  9012 -> 12
+
+int number = InputInt("Введите число");
+SumDigitsNumber();
+
+int InputInt(string message)    //вводим необходимые данные
+{
+    System.Console.Write(message + " > ");
+    string? inputValue = System.Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+void SumDigitsNumber()     //функция суммирования цифр в числе
+{
+    int index = number;     //переменная для вывода в консольную строку.
+    int size = 0;           //переменная для фиксации размера числа (кол-во цифр в числе)
+    int result = 0;
+    int sum = 0;
+
+    if (number <= 9)
+    {
+        sum = number;
+        System.Console.WriteLine($"Сумма цифер числа {index} равна: {sum}");
+        return;
+    }
+
+    while (number > 0)   //определяем размер числа
+    {
+        for (int i = 0; i < size; i++)  //суммируем цифры в числе
+        {
+            result = number % 10;
+            sum += result;
+            number /= 10;
+        }
+        size++;
+    }
+    System.Console.WriteLine($"Сумма цифер числа {index} равна: {sum}");
+}
